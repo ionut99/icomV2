993b6b4c83e29ef23b3e07fba9c77dee3f61118f
Ionut <ionut.pavel@mta.ro>
2021-12-27 17:15:06 +0200

    Prima adaugare a proiectului

---
+++ server/server.js
@@ -0,0 +1,154 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const utils = require('./utils');
+const mysql = require("mysql");
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// create DataBase Connection
+var config = 
+{
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'icomdatabase'
+}
+
+const db = new mysql.createConnection(config);
+
+// database user details
+var userData = {
+  userId: "",
+  passwordU: "",
+  name: "",
+  username: "",
+  isAdmin: false
+};
+
+// enable CORS
+app.use(cors());
+// parse application/json
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//middleware that checks if JWT token exists and verifies it if it does exist.
+//In all future routes, this helps to know if the request is authenticated or not.
+app.use(function (req, res, next) {
+  // check header or url parameters or post parameters for token
+  var token = req.headers['authorization'];
+  if (!token) return next(); //if no token, continue
+
+  token = token.replace('Bearer ', '');
+  jwt.verify(token, process.env.JWT_SECRET, function (err, user) {
+    if (err) {
+      return res.status(401).json({
+        error: true,
+        message: "Invalid user."
+      });
+    } else {
+      req.user = user; //set the user to req so other routes can use it
+      next();
+    }
+  });
+});
+
+
+// request handlers
+app.get('/', (req, res) => {
+  if (!req.user) return res.status(401).json({ success: false, message: 'Invalid user to access it.' });
+  res.send('Welcome to the Node.js Tutorial! - ' + req.user.name);
+});
+
+
+// validate the user credentials
+app.post('/users/signin', function (req, res) {
+  const user = req.body.username;
+  const pwd = req.body.password;
+
+  // return 400 status if username/password is not exist
+  if (!user || !pwd) {
+    return res.status(400).json({
+      error: true,
+      message: "Username or Password required."
+    });
+  }
+
+  db.query(
+    "SELECT * FROM IUsers WHERE username = ? AND password = ?",
+    [user, pwd],
+    (err, result) => {
+      if (err) {
+          // error sql statement
+        return res.status(403).json({
+            error: true,
+            message: "Error select dataBase"
+          });
+      }
+      if (result.length > 0) {
+        // if succeed complete data with the result from database
+        
+        userData.userId = result[0].userId;
+        userData.name = result[0].Name;
+        userData.username = result[0].Username;
+        userData.passwordU = result[0].Password;
+        userData.isAdmin = result[0].IsAdmin;
+
+        // generate token
+        const token = utils.generateToken(userData);
+        // get basic user details
+        const userObj = utils.getCleanUser(userData);
+        // return the token along with user details
+        return res.json({ user: userObj, token });
+
+      } else {
+          // return 401 status if the credential is not match.
+        return res.status(401).json({
+            error: true,
+            message: "Username or Password is Wrong."
+          });
+      }
+    }
+  )
+});
+
+
+// verify the token and return it if it's valid
+app.get('/verifyToken', function (req, res) {
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token;
+  if (!token) {
+    return res.status(400).json({
+      error: true,
+      message: "Token is required."
+    });
+  }
+  // check token that was passed by decoding token using secret
+  jwt.verify(token, process.env.JWT_SECRET, function (err, user) {
+    if (err) return res.status(401).json({
+      error: true,
+      message: "Invalid token."
+    });
+
+    // return 401 status if the userId does not match.
+    if (user.userId !== userData.userId) {
+      return res.status(401).json({
+        error: true,
+        message: "Invalid user."
+      });
+    }
+    // get basic user details
+    var userObj = utils.getCleanUser(userData);
+    return res.json({ user: userObj, token });
+  });
+});
+
+app.listen(port, () => {
+  console.log('Server started on: ' + port);
+});
