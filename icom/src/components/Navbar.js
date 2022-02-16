ed6f5d85ae07520bd9e6ded1a5b1a53428a3de0a
Ionut <ionut.pavel@mta.ro>
2022-02-16 23:19:32 +0200

    Restilizare Sidebar

--- icom/src/components/Navbar.js
+++ icom/src/components/Navbar.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { SidebarData } from './SidebarData';
-import '../cssFiles/Navbar.css';
-import { IconContext } from 'react-icons';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { SidebarData } from "./SidebarData";
+import "../cssFiles/Navbar.css";
+import { IconContext } from "react-icons";
+
+import logo from "../images/logo.png";
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
-  
 
   return (
     <>
-      <IconContext.Provider value={{ color: '#fff' }}>
-        <nav className={'nav-menu'}>
-          <ul className='nav-menu-items' onClick={showSidebar}>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <nav className={"nav-menu"}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <img className="logo_picture" src={logo} alt="logo jmecher" />
             {SidebarData.map((item, index) => {
               return (
                 <li key={index} className={item.cName}>
                   <Link to={item.path}>
-                    <div className='icon'>
-                        {item.icon}
-                    </div>
+                    <div className="icon">{item.icon}</div>
                     <span>{item.title}</span>
                   </Link>
                 </li>
