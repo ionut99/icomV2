3b2746fca60c2381dc01c07a0a380eb949354dd2
Ionut <ionut.pavel@mta.ro>
2022-02-18 20:56:41 +0200

    Start chat window front end

--- icom/src/pages/Chat.js
+++ icom/src/pages/Chat.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Navbar from '../components/Navbar';
+import "../cssFiles/chat.css";
 
 function Chat() {
   return (
-    <div>
+    <div className="page-content">
       <Navbar />
-      Chat
+      <div className="chat-page">
+        <div className="left-section">
+
+        </div>
+        <div className="right-section">
+
+        </div>
+      </div>
     </div>
   );
 }
