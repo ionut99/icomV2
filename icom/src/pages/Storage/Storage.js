741285ee943f80dba989f4c09538979105d9f7fe
Ionut <ionut.pavel@mta.ro>
2022-04-16 22:37:33 +0300

    Start Create folderReducer.js

--- icom/src/pages/Storage/Storage.js
+++ icom/src/pages/Storage/Storage.js
@@ -3,15 +3,18 @@ import { Container } from "react-bootstrap";
 import AddFolderButton from "./AddFolderButton";
 import Navbar from "../../components/Navbar/Navbar";
 
-import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
+import { useFolder } from "../../reducers/folderReducer";
 
 function Storage() {
+  const { folder } = useFolder();
+
   return (
     <>
       <Navbar />
       <Container fluid>
-        <AddFolderButton />
+        <AddFolderButton currentFolder={folder} />
       </Container>
     </>
   );
