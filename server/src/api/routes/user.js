34fd5c998913f8eb195cdaa11a86237fc546d420
Ionut <ionut.pavel@mta.ro>
2022-06-20 19:14:45 +0300

    repaire

---
+++ server/src/api/routes/user.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+
+const { SignInUser, LogOutUser, VerifyToken } = require("../controllers/Auth");
+const {
+  getNewUserChatList,
+  GetUserDetails,
+  EditUserAccountAsync,
+} = require("../controllers/User");
+
+//
+const {
+  InserNewUserAccount,
+  adminGetUserList,
+} = require("../controllers/Admin");
+//
+
+const { GetNOTPartList } = require("../controllers/Room");
+const { UpdateProfilePicture } = require("../controllers/Files");
+const { authMiddleware } = require("../middlewares/Auth");
+
+router.post("/signin", SignInUser);
+router.post("/logout", LogOutUser);
+router.post("/verifyToken", VerifyToken);
+
+// router.post("/search", authMiddleware, GetUserSearchList);
+// router.post("/updatePicture", authMiddleware, UpdateProfilePicture);
+router.post("/newchat", getNewUserChatList);
+router.post("/search", adminGetUserList);
+router.post("/toadd", GetNOTPartList);
+router.post("/updatePicture", UpdateProfilePicture);
+router.post("/details", GetUserDetails);
+router.post("/newuser", InserNewUserAccount);
+router.post("/edit", EditUserAccountAsync);
+module.exports = router;
